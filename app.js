e3c1a2eb8590fb2aadf55bad40bb648ecf2b400b
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-09-30 20:43:43 +0100

    feat: Add environment variable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const indexRouter = require("./routes/index");
 
@@ -6,6 +7,6 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/", indexRouter);
-app.listen(3000, () => console.log("Server listening on port 3000!"));
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
