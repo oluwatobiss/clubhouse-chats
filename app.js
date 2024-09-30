05724e7015c17bd418d19aa136b0035870fb5d56
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-09-30 14:18:19 +0100

    feat: Create server

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("<h1>Hello there!</h1>"));
+app.listen(3000, () => console.log("Server listening on port 3000!"));
