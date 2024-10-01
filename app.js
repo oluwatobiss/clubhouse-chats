bec160b4333b82cb03d33638dfb29389473b0c69
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-10-01 12:44:00 +0100

    style: Design homepage

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("node:path");
 const indexRouter = require("./routes/index");
 
 const app = express();
@@ -7,6 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
