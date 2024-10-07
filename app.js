149e7049153ad407ea280f5c3372d2f548366683
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-10-07 22:34:06 +0100

    feat: Make session cookie persistent

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
   })
 );
 app.use(passport.session());
